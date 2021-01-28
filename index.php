f699f57292b8cad33d4ad0664650ee0fca51b513
LJM12914 <LJM12914@outlook.com>
2021-01-28 16:07:11 +0800

    update

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>jmlink</title>
+</head>
+<body>
+	<?php
+    $source = $_REQUEST['input'];
+	echo $source;
+	?>
+</body>
+</html>
